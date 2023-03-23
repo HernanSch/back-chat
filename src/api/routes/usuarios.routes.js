b3e59c0317e148eb39553ccfca697fbdd9f6f043
Hernan <hschiavonr@gmail.com>
2023-03-23 09:32:05 +0100

    vercel fix

--- src/api/routes/usuarios.routes.js
+++ src/api/routes/usuarios.routes.js
@@ -2,6 +2,12 @@ const express = require("express");
 const {postRegisterUsuarios, postLoginUsuarios, getUsuarios, postLogout, usersConnected, updateUserConnectedStatus} = require("../controllers/usuarios.controllers");
 const router = express.Router();
 
+// Agrega el middleware aquí
+router.use(function(req, res, next) {
+  console.log('Este middleware se ejecutará en todas las rutas de usuarios.routes.js');
+  next();
+});
+
 router.get("/get",getUsuarios)
 router.get("/conectados",usersConnected)
 router.post("/register",postRegisterUsuarios)
@@ -10,4 +16,3 @@ router.post("/logout",postLogout)
 router.put("/updateusers/:id",updateUserConnectedStatus)
 
 module.exports = router;
-
